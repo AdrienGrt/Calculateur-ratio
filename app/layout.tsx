10a133b50094c8461f2c1c0454103691ba88e823
Adrien Gardrat <adrien@MacBook-Air-de-Adrien.local>
2025-04-18 15:18:43 +0200

    « update »

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,31 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
-}
+  title: "HorizonContrast | Contrast Checker",
+  description: "Vérificateur de contraste pour les couleurs de texte et d'arrière-plan",
+  
+  keywords: "HorizonContrast, vérificateur de contraste, accessibilité, couleurs, texte, arrière-plan",
+  authors: [{ name: "Perspective Horizon" }],
+  creator: "Perspective Horizon",
+  publisher: "Perspective Horizon",
+  openGraph: {
+    type: "website",
+    locale: "fr_FR",
+    url: "https://horizonkey.perspectivehorizon.fr/",
+    title: "HorizonContrast | Contrast Checker",
+    description: "Vérificateur de contraste pour les couleurs de texte et d'arrière-plan",
+    siteName: "HorizonContrast",
+    images: [{
+      url: "/og-image.png",
+      width: 1200,
+      height: 630,
+      alt: "HorizonKey Preview"
+    }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "HorizonKey | Password Generator",
+    description: "Générateur de mots de passe sécurisés par Perspective Horizon",
+    images: ["/og-image.png"],
+  }
+};
